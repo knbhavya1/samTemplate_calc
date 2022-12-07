bfeb81b38c7f659f891d2ebfdc17bf1d4e09a9e4
KN Bhavya <bhavya.kn@datagrokr.com>
2022-12-07 18:14:39 +0530

    changed test handler file

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -7,19 +7,19 @@ from hello_world import app
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(app.add(1,5),6)
-        self.assertEqual(app.add(-2,5),3)
-        self.assertEqual(app.add(-2,-3),-5)
+        self.assertEqual(app.add(10,5),15)
+        self.assertEqual(app.add(-1,1),0)
+        self.assertEqual(app.add(-1,-1),-2)
 
     def test_sub(self):
-        self.assertEqual(app.sub(12,2),10)
-        self.assertEqual(app.sub(-1,5),-6)
-        self.assertEqual(app.sub(-2,-1),-1)
+        self.assertEqual(app.sub(10,5),5)
+        self.assertEqual(app.sub(-1,1),-2)
+        self.assertEqual(app.sub(-1,-1),0)
 
     def test_multiply(self):
-        self.assertEqual(app.multiply(5,5),25)
-        self.assertEqual(app.multiply(-2,5),-10)
-        self.assertEqual(app.multiply(-2,-3),6) 
+        self.assertEqual(app.multiply(10,5),5)
+        self.assertEqual(app.multiply(-1,1),-1)
+        self.assertEqual(app.multiply(-1,-1),1) 
 
 if __name__ == '__main__':
      unittest.main()               
