e5c2ce98203775361a82ee144190a08fee164bc6
KN Bhavya <bhavya.kn@datagrokr.com>
2022-12-07 19:16:55 +0530

    changed handler function

--- hello_world/app.py
+++ hello_world/app.py
@@ -7,19 +7,21 @@ def lambda_handler(event, context):
     num1= event["num1"]
     num2= event["num2"]
 
+    print(add(num1,num2))
+    print(sub(num1,num2))
+    print(multiply(num1,num2))  
+
     
 
-def add(num1,num2):
+def add(num1, num2):
     return num1 + num2    
 
-def sub(num1,num2):
+def sub(num1, num2):
     return num1 - num2
 
-def multiply(num1,num2):
+def multiply(num1,  num2):
     return num1 * num2   
 
-print(add(num1,num2))
-print(sub(num1,num2))
-print(multiply(num1,num2))                 
+                
 
     
